48a2f7a67fde739a5ff4efe8f8265e32054f057f
Leonel Maximo Vargas Ramirez <maxramirez@MacBook-Pro-de-Leonel.local>
2023-09-23 01:37:45 -0400

    busqueda de imagen

--- src/views/client/ImageView.tsx
+++ src/views/client/ImageView.tsx
@@ -1,12 +1,17 @@
 
+import { useImageStore } from '@/hooks';
 import './styles.css';
-import { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export const ImageView = () => {
     const appRef = useRef<HTMLDivElement | null>(null);
     const inputRef = useRef<HTMLInputElement | null>(null);
     const buttonRef = useRef<HTMLButtonElement | null>(null);
     const layerRef = useRef<HTMLDivElement | null>(null);
+
+
+    const { getImage } = useImageStore();
+    const [image, setImage] = useState<string | null>(null);
 
     const toggleApp = () => {
         if (appRef.current) {
@@ -51,28 +56,46 @@ export const ImageView = () => {
         }
     };
 
+    const handleSubmited = async (event:any) => {
+        event.preventDefault();
+        if(inputRef.current!.value) {
+            const imageData = await getImage(inputRef.current!.value);
+            setImage(imageData);
+        }
+    }
+
     return (
         <div id="app-cover">
-            <div id="app" ref={appRef}>
-                <form method="get" action="">
-                    <div id="f-element">
-                        <div id="inp-cover">
-                            <input
-                                type="text"
-                                name="query"
-                                placeholder="Ingresa el código de tu foto"
-                                autoComplete="off"
-                                ref={inputRef}
-                            />
-                        </div>
+          <div id="app" ref={appRef}>
+            <form onSubmit={handleSubmited}>
+              <div id="f-element">
+                <div id="inp-cover">
+                  <input
+                    type="text"
+                    name="query"
+                    placeholder="Ingresa el código de tu foto"
+                    autoComplete="off"
+                    ref={inputRef}
+                  />
+                </div>
+              </div>
+              <div className="container">
+                <div className="container-image">
+                  {image !== null && (
+                    <div className="image-container">
+                      <img src={image} alt="Imagen" />
+                      <button className="button-download"></button>
                     </div>
-                    <button type="submit" className="shadow" ref={buttonRef}>
-                        <i className="fas fa-search"></i>
-                    </button>
-                </form>
-            </div>
-            <div id="layer" title="Click the blue area to hide the form" ref={layerRef}></div>
-            <div id="init" onClick={toggleApp}></div>
+                  )}
+                </div>
+                <button type="submit" className="shadow" ref={buttonRef}>
+                  <i className="fas fa-search"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+          <div id="layer" title="Click the blue area to hide the form" ref={layerRef}></div>
+          <div id="init" onClick={toggleApp}></div>
         </div>
-    );
+      );
 };
